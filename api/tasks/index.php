9a3dc903f1f9ed67a56669dab0383c1b7b40429c
BiagioCaputo <scebiagio98@gmail.com>
2024-02-22 19:28:56 +0100

    completed the function to add task

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -8,11 +8,27 @@ $source_path = __DIR__ . '/../../database/tasks.json';
 //prendo i contenuti del file json
 $data = file_get_contents($source_path);
 
+//salvo in task i data
+$tasks = $data;
+
 //converto in array php
-$tasks = json_decode($data, true);
+$tasks = json_decode($tasks, true);
+
+//controllo se ho un nuovo task
+$newTask = $_POST['task'] ?? '';
+
+//se ce l'ho
+if($newTask) {
+    //aggiungo il task
+    $tasks[] = $newTask;
+}
 
 //riconverto in json
 $tasks = json_encode($tasks);
+
+//sovrascrivo il vecchio array tasks con quello aggiornato
+
+file_put_contents($source_path, $tasks);
 
 //stampo i task riconvertiti in jason
 echo $tasks;
--- my_app/js/script.js
+++ my_app/js/script.js
@@ -5,7 +5,26 @@ const endpoint = 'http://localhost/boolean/php-todo-list-json/api/tasks/';
 const app = createApp({
     data: () => ({
         tasks: [],
+        newTask: '',
     }),
+
+    methods: {
+        addTask() {
+            const data = {
+                task: {
+                    id: this.tasks.length + 1,
+                    text: this.newTask,
+                    done: false
+                }
+            };
+            const config = { headers: { 'Content-Type': 'multipart/form-data' } }
+
+            axios.post(endpoint, data, config).then(res => {
+                this.tasks = res.data;
+                this.newTask = '';
+            })
+        }
+    },
 
     created() {
         axios.get(endpoint).then(res => {
